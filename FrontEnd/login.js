e509671a9d4cc2151fb06e881f83e4d358e3ccad
EstiaanJ <xero416@gmail.com>
2023-03-05 16:47:27 +0800

    Security Updates

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -6,13 +6,36 @@ function hashText(inputText) {
   function createEndPoint() {
     var password = document.getElementById("password").value;
     var username = document.getElementById("username").value;
-    var endpoint = "/account/" + username + "/" + hashText(password + username);
     
-    // Add query parameters to URL
-    var url = "account.html?username=" + encodeURIComponent(username) + "&hash=" + encodeURIComponent(endpoint);
+    // Validate user input
+    if (!/^[a-zA-Z0-9]+$/.test(username)) {
+        alert("Invalid username");
+        return;
+    }
+    if (!/^[a-zA-Z0-9@#$%^&+=]+$/.test(password)) {
+        alert("Invalid password");
+        return;
+    }
+    
+    // Encode user input
+    var encodedUsername = encodeURIComponent(username);
+    var encodedPassword = encodeURIComponent(password + username);
+    
+    // Hash password
+    var hashedPassword = hashText(encodedPassword);
+    
+    // Build endpoint URL
+    var endpoint = "/account/" + encodedUsername + "/" + hashedPassword;
+    
+    // Build query string
+    var urlParams = new URLSearchParams();
+    urlParams.append("username", encodedUsername);
+    urlParams.append("hash", endpoint);
+    
+    // Build URL with query string
+    var url = "account.html?" + urlParams.toString();
     
     // Open account.html page with query parameters
     window.open(url, "_self");
-  }
-  
+}
   
--- FrontEnd/transfer.js
+++ FrontEnd/transfer.js
@@ -1,30 +1,42 @@
-console.log("transfer.js loaded");
+
 
 function transfer() {
-    var to_acc_username = document.getElementById("to_acc_username").value;
-    var amount = document.getElementById("amount").value;
-    var from_acc_name = document.getElementById("username").textContent;
+  var to_acc_username = document.getElementById("to_acc_username").value;
+  var amount = document.getElementById("amount").value;
+  var from_acc_name = document.getElementById("username").textContent;
 
-    console.log("Transfering " + amount + " from " + from_acc_name + " to " + to_acc_username);
-    
-    // Make a POST request to the /transfer endpoint with the from_acc_username and amount values
-    $.ajax({
-      url: "http://51.161.163.66:44658/transfer",
-      type: "POST",
-      dataType: "json",
-      data: JSON.stringify({from_acc_name:from_acc_name,to_acc_username:to_acc_username, amount: amount }),
-      success: function(result) {
-        // If the request is successful, update the account summary and transaction history
-        //updateAccountSummary();
-        //updateTransactionHistory();
-        console.log("Transfer successful");
-        window.location.reload();
-      },
-      error: function(xhr, status, error) {
-        // If the request fails, display an error message
-        alert("Error transferring funds: " + xhr.responseText);
-        window.location.reload();
-      }
-    });
+  // Validate user input
+  if (!/^[a-zA-Z0-9]+$/.test(to_acc_username)) {
+      alert("Invalid recipient account username");
+      return;
+  }
+  if (isNaN(amount) || amount <= 0) {
+      alert("Invalid transfer amount");
+      return;
   }
   
+  // Encode user input
+  var encodedToUsername = encodeURIComponent(to_acc_username);
+  var encodedAmount = encodeURIComponent(amount);
+  var encodedFromUsername = encodeURIComponent(from_acc_name);
+  
+  // Make a POST request to the /transfer endpoint with the encoded user input
+  $.ajax({
+    url: "http://51.161.163.66:44658/transfer",
+    type: "POST",
+    dataType: "json",
+    data: JSON.stringify({from_acc_name: encodedFromUsername, to_acc_username: encodedToUsername, amount: encodedAmount }),
+    success: function(result) {
+      // If the request is successful, update the account summary and transaction history
+      //updateAccountSummary();
+      //updateTransactionHistory();
+      console.log("Transfer successful");
+      window.location.reload();
+    },
+    error: function(xhr, status, error) {
+      // If the request fails, display an error message
+      alert("Error transferring funds: " + xhr.responseText);
+      window.location.reload();
+    }
+  });
+}
